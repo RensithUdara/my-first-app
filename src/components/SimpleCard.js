8345703337a76e55aab84cb2c33a39578727d483
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-09-29 03:08:53 +0530

    update ui

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,5 +1,16 @@
+import './SimpleCard.css'
+
 function SimpleCard() {
-    return <div>My Simple Card</div>
+    return (
+
+        <div className='card-container'>
+            <h2>Introduction</h2>
+            <p>
+            Hold the dummy, and gently touch the skin around your baby's lips and cheeks with the teat. Allow your little one to feel the dummy's material against their skin. This will help trigger the rooting reflex and your baby will turn their face towards the stimulus. Observe your baby's behavior.
+            </p>
+            <button>View More</button>
+        </div>
+    );
 }
 
 export default SimpleCard;
