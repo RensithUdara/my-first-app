ec07a4449ce64c481b011bc44036656f59f80d9a
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-10-05 21:49:28 +0530

    In-line styles

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ function App() {
   const url = 'https://images.unsplash.com/photo-1507146426996-ef05306b995a?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHVwcHl8ZW58MHx8MHx8fDA%3D' ;
   return (
     <>
-    <img src='{url}' />
+
+    {/* <img src='{url}' /> */}
+
+    <h style = {{ 
+      color : "white"
+     }}>Hello World !</h>
+
     </>
+
   // <div>
   //   <SimpleCard/> 
   //   <NavBar/>
